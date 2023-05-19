15b834a4292274ae7b883c1176f9a3054447a93d
Gagik Kazandjyan <gkazanjian01@gmail.com>
2023-05-19 20:24:58 +0400

    Solved (electronic-watch)

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,13 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int hrs = seconds / 3600;
+        if (hrs >= 24){
+            hrs = Math.abs(24-(seconds / 3600));
+        }
+        int min = (seconds % 3600) / 60;
+        int sec = seconds % 60;
 
-
+        System.out.printf("%d:%02d:%02d", hrs, min, sec);
     }
 }
